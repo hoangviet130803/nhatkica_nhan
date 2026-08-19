ade204bf7a9995fd8f2432c33d6becf1f3914be8
HoàngViệt <144772023+hoangviet130803@users.noreply.github.com>
2026-08-19 16:13:09 +0700

    Update API base URL to mock API service

--- src/api/API.js
+++ src/api/API.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:9999";
+// const BASE_URL = "http://localhost:9999";
+const BASE_URL = "https://6a856dba9c451dc67a6398db.mockapi.io/api";
+
 const api = axios.create({
     baseURL: BASE_URL,
     headers: { "Content-Type": "application/json" },
